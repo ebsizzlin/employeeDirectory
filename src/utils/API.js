702d8682e13d636c140ccaa39a1805fc7e98a183
Ella Thompson <fang98t@gmail.com>
2021-01-31 16:28:14 -0500

    api.js take 2

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=50");
+  search: function() {
+       return axios.get(BASEURL);
   }
 };
-
-//why doesn't it like this?
