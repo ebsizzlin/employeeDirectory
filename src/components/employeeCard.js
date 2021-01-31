23ac285a6738a0e80f246a18b179f71a741262a8
Ella Thompson <fang98t@gmail.com>
2021-01-31 16:43:52 -0500

    import and func

--- src/components/employeeCard.js
+++ src/components/employeeCard.js
@@ -1,5 +1,17 @@
 //import
+import React from "react";
 
 //employeecard func
+function EmployeeCard(props) {
+    return (
+        <tr>
+            <th scope="row"><img alt={props.firstName} src={props.picture} /></th>
+            <td >{props.firstName}</td>
+            <td >{props.lastName}</td>
+            <td >{props.email}</td>
+            <td >{props.phone}</td>
+        </tr>
+    );
+}
 
 //export
