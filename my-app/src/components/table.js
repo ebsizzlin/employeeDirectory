171e1c5acb164299d80c87fa6e565222b9d18a3f
Ella Thompson <fang98t@gmail.com>
2021-02-02 16:18:10 -0500

    tablejs pseudocode

---
+++ my-app/src/components/table.js
@@ -0,0 +1,18 @@
+//import
+//import utils api
+//import search
+//import styles
+
+//npm DateFormat???
+
+//api call randomuser
+
+//handle input searchbar
+
+//sort by first
+
+//sort by last
+
+//render JSX func
+
+//export
