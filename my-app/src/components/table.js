4635d067a30a0501084194b1bcc2f1d4621ca844
Ella Thompson <fang98t@gmail.com>
2021-02-02 16:21:43 -0500

    imports, dateformat attempt, componentdidmount for randomuser

--- my-app/src/components/table.js
+++ my-app/src/components/table.js
@@ -1,11 +1,31 @@
 //import
+import React from "react";
 //import utils api
+import API from "../utils/api.js"
 //import search
+import Search from "../components/searchBar"
 //import styles
+import "../styles/table.css"
 
 //npm DateFormat???
+import DateFormat from 'dateformat';
 
-//api call randomuser
+class Table extends React.Component {
+
+    state = {
+        sortOrder: "",
+        results: [],
+        search: ""
+    }
+
+    //api call randomuser
+    componentDidMount() {
+        API.ApiSearch()
+        .then(res => {
+            this.setState({ results: res.data.results })
+            console.log(this.state.results)
+        }).catch(err => console.log(err))
+    }
 
 //handle input searchbar
 
