8ffc717d67c951efbe1bf697f1d4979d047933e8
Ella Thompson <fang98t@gmail.com>
2021-02-02 16:26:43 -0500

    render func first name

--- my-app/src/components/table.js
+++ my-app/src/components/table.js
@@ -98,7 +98,20 @@ class Table extends React.Component {
                   </tr>
                 </thead>
 
-                {/* first name */}
+                { //First Name sort
+                this.state.results && this.state.results.map(item =>
+                    item.name.first.toLowerCase().includes(this.state.search) ?
+                        <tbody key={item.login.uuid}>
+                            <tr>
+                            <td ><img src={item.picture.thumbnail} className="rounded-circle" alt="thumbnail" /></td>
+                            <td >{item.name.first}</td>
+                            <td >{item.name.last}</td>
+                            <td >{item.phone}</td>
+                            <td >{item.email}</td>
+                            <td>{DateFormat(item.dob.date, "mediumDate")}</td>  
+                            </tr>
+                        </tbody>
+
                 {/* last name */}
 
 {/* //export */}
