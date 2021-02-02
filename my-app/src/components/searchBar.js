a82ce62c60a626f89d9de57be5f1b4cd2ec95c8f
Ella Thompson <fang98t@gmail.com>
2021-02-02 16:15:58 -0500

    searchbar js written

--- my-app/src/components/searchBar.js
+++ my-app/src/components/searchBar.js
@@ -1,7 +1,31 @@
 //import
-
+import React from "react";
 //import styles
+import "../styles/searchBar.css"
 
 //search props func
+const SearchBar = (props) => {
+    //const context = useContext(DataAreaContext)
+    
+      return (
+        <form>
+        <div className="form-group search-widget">
+          <div className="input-group mb-3">
+      
+            <input
+              onChange={props.handleInputChange}
+              value={props.search}
+              name="search"
+              type="text"
+              className="form-control"
+              placeholder='Search directory for Employee'
+              id="search"
+            />
+          </div>
+        </div>
+      </form>
+      );
+    }
 
 //export
+export default SearchBar;
